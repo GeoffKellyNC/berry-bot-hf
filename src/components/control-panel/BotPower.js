345863b7c81daac6110285f1ae508b2d16c6dd5a
Geoff Kelly <65321097+GeoffKellyNC@users.noreply.github.com>
2022-06-05 16:16:11 -0400

    scaled for mobile UI

--- src/components/control-panel/BotPower.js
+++ src/components/control-panel/BotPower.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function BotPower({startBot, botData}) {
+function BotPower({startBot}) {
   return (
     <BotPowerStyled>
          <button 
